b08535f0168e5c67f674e0d9a33c589f0f2ef4f4
Victor Powell <vicapow@gmail.com>
2023-10-19 13:11:05 -0700

    update heaviside

--- src/HeavisideActivation.tsx
+++ src/HeavisideActivation.tsx
@@ -4,50 +4,40 @@ import { LinePath } from "@visx/shape";
 import { scaleLinear } from "@visx/scale";
 import { AxisLeft, AxisBottom } from "@visx/axis";
 import { GridRows, GridColumns } from "@visx/grid";
-import { CityTemperature } from "@visx/mock-data/lib/mocks/cityTemperature";
+import { Point } from "./bezier";
 
 export const background = "#f3f3f3";
 
-const cityTemperature: Array<CityTemperature> = [
-  {
-    date: "1",
-    "New York": "63.4",
-    "San Francisco": "62.7",
-    Austin: "72.2",
-  },
-  {
-    date: "2",
-    "New York": "58.0",
-    "San Francisco": "59.9",
-    Austin: "67.7",
-  },
-  {
-    date: "3",
-    "New York": "53.3",
-    "San Francisco": "59.1",
-    Austin: "69.4",
-  },
-];
+function heaviside(d: number) {
+  if (d < 0) {
+    return 0;
+  }
+  if (d > 0) {
+    return 1;
+  }
+  return 0.5;
+}
+
+const data: Array<Point> = [];
+
+let start = -2;
+let end = 2;
+let samples = 100;
+for (let i = 0; i < samples; i++) {
+  let step = (end - start) / (samples - 1);
+  let input = start + step * i;
+  data.push([input, heaviside(input)]);
+}
 
 // accessors
-const date = (d: CityTemperature) => Number(d.date);
-const ny = (d: CityTemperature) => Number(d["New York"]);
-const sf = (d: CityTemperature) => Number(d["San Francisco"]);
+const x = (d: Point) => Number(d[0]);
+const y = (d: Point) => Number(d[1]);
 
 // scales
 const xScale = scaleLinear<number>({
-  domain: [
-    Math.min(...cityTemperature.map(date)),
-    Math.max(...cityTemperature.map(date)),
-  ],
+  domain: [-2, 2],
 });
-const yScale = scaleLinear<number>({
-  domain: [
-    Math.min(...cityTemperature.map((d) => Math.min(ny(d), sf(d)))),
-    Math.max(...cityTemperature.map((d) => Math.max(ny(d), sf(d)))),
-  ],
-  nice: true,
-});
+const yScale = scaleLinear<number>({ domain: [-0.5, 1.5], nice: true });
 
 const defaultMargin = { top: 40, right: 30, bottom: 50, left: 40 };
 
@@ -82,12 +72,12 @@ export default function Theshold({
         rx={14}
       />
       <Group left={margin.left} top={margin.top}>
-        <GridRows scale={yScale} width={xMax} height={yMax} stroke="#e0e0e0" />
+        <GridRows scale={yScale} width={xMax} height={yMax} stroke="#f0f0f0" />
         <GridColumns
           scale={xScale}
           width={xMax}
           height={yMax}
-          stroke="#e0e0e0"
+          stroke="#f0f0f0"
         />
         <line x1={xMax} x2={xMax} y1={0} y2={yMax} stroke="#e0e0e0" />
         <AxisBottom top={yMax} scale={xScale} numTicks={width > 520 ? 10 : 5} />
@@ -96,11 +86,48 @@ export default function Theshold({
           Y Axis
         </text>
         <LinePath
-          data={cityTemperature}
-          x={(d) => xScale(date(d)) ?? 0}
-          y={(d) => yScale(ny(d)) ?? 0}
-          stroke="#222"
+          className="stroke-sky-500"
+          data={
+            [
+              [-2, 0],
+              [0, 0],
+            ] as Array<Point>
+          }
+          x={(d) => xScale(x(d)) ?? 0}
+          y={(d) => yScale(y(d)) ?? 0}
           strokeWidth={1.5}
+        />
+        <LinePath
+          className="stroke-sky-500"
+          data={
+            [
+              [0, 1],
+              [2, 1],
+            ] as Array<Point>
+          }
+          x={(d) => xScale(x(d)) ?? 0}
+          y={(d) => yScale(y(d)) ?? 0}
+          strokeWidth={1.5}
+        />
+        <circle
+          className="stroke-sky-500"
+          cx={xScale(0)}
+          cy={yScale(0)}
+          r="4"
+          fill="white"
+        />
+        <circle
+          className="stroke-sky-500"
+          cx={xScale(0)}
+          cy={yScale(1)}
+          r="4"
+          fill="white"
+        />
+        <circle
+          className="stroke-sky-500 fill-sky-200"
+          cx={xScale(0)}
+          cy={yScale(0.5)}
+          r="4"
         />
       </Group>
     </g>
